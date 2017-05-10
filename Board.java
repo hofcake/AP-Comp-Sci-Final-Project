95d67486f5056bb652c029722f6e13824a31a114
Jamie Hofer <jamie.k.hofer@gmail.com>
2017-05-10 08:19:41 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -17,7 +17,7 @@ public class Board
     public void printBoard(){
 		for(int i = 0; i < board.length; i++){
 			for(int j = 0; j < board[0].length; j++){
-				System.out.Print(board[i][j] + " ");
+				System.out.print(board[i][j] + " ");
 			}
 			System.out.println();
     	}
