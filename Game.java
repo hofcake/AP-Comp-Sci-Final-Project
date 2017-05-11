22f1facf01801a961b0e857003031824b331cb95
jacob3578 <jessig@pennington.org>
2017-05-11 08:27:01 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,19 @@
 public class Game
 {
-    public static createBoard(){
-        
-    }
+        public void promptForInput()
+    {
+        //Declare and initialize a scanner object called keyboard
+        Scanner keyboard = new Scanner(System.in);
+        String userEntry = "";
+        while(! userEntry.equals("*"))
+        {
+            System.out.print("\f");
+            System.out.print(userEntry);
+            System.out.print("Enter Letter to Guess or * to exit : ");
+            userEntry = keyboard.nextLine();
+            //convert userEntry to be trimmed and all upper case
+            userEntry = userEntry.trim().toUpperCase();
+        }
+        System.out.print("\nGood Bye!");
+    }  
 }
