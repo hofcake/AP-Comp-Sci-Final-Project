6ff59ad804824fd7ef5e6e4b8d39aad9921d290b
Jamie Hofer <jamie.k.hofer@gmail.com>
2017-05-11 08:50:18 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -5,8 +5,10 @@ public class Board
     public void newBoard(int size){
         board = new int[size][size];
     }
+
     public void newMark(int x, int y, int type){board[board.length-y-1][x] = type;} //0 = nothing, 1 = x, 2 = O
     public void removeMark(int x, int y){board[board.length-y-1][x] = 0;}
+
     public void clear(){
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
@@ -14,11 +16,13 @@ public class Board
             }
         }
     }
+
     public boolean checkMark(int x, int y){
         if(board[y][x] == 0)
             return true;
         return false;
     }
+
     public void printBoard(){
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
@@ -35,48 +39,51 @@ public class Board
         }
         System.out.println("\n");
     }
+
     public boolean checkHorizontal(int sign){
-	boolean win = true;
-	for(int i = 0; i < board.length; i++){
-		win = true;
-		for(int j = 0; j < board[0].length; j++){
-			if((board[i][j] == sign)&&(win))
-				win = true;
-			else
-				win = false;
-			}
-		}
-		return win;
+        boolean win = true;
+        for(int i = 0; i < board.length; i++){
+            win = true;
+            for(int j = 0; j < board[0].length; j++){
+                if((board[i][j] == sign)&&(win))
+                    win = true;
+                else
+                    win = false;
+            }
+        }
+        return win;
     }
-	public boolean checkVertical(int sign){
-	boolean win = true;
-	for(int i = 0; i < board.length; i++){
-		win = true;
-		for(int j = 0; j < board[0].length; j++){
-			if((board[j][i] == sign)&&(win))
-				win = true;
-			else
-				win = false;
-			}
-		}
-		return win;
+
+    public boolean checkVertical(int sign){
+        boolean win = true;
+        for(int i = 0; i < board.length; i++){
+            win = true;
+            for(int j = 0; j < board[0].length; j++){
+                if((board[j][i] == sign)&&(win))
+                    win = true;
+                else
+                    win = false;
+            }
+        }
+        return win;
     }
-	public boolean checkDiagonal(int sign){
-		boolean win = true;
-		int index = 0;
-		while(index < board.length){
-			if(board[index][index] != sign)
-				win = false;
-			index++;
-		}
-		if(win == true)
-			return true;
-		index = board.length-1;
-		while(index >= 0){
-			if(board[index][board[].length-1-index] != sign)
-				win = false;
-			index--;
-		}
-		return win;
-	}
+
+    public boolean checkDiagonal(int sign){
+        boolean win = true;
+        int index = 0;
+        while(index < board.length){
+            if(board[index][index] != sign)
+                win = false;
+            index++;
+        }
+        if(win == true)
+            return true;
+        index = board.length-1;
+        while(index >= 0){
+            if(board[index][board[0].length-1-index] != sign)
+                win = false;
+            index--;
+        }
+        return win;
+    }
 }
