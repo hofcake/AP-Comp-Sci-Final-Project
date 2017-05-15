895ddab8c284c37954eeae9a4b7b08bdcf34b28b
Jamie Hofer <jamie.k.hofer@gmail.com>
2017-05-15 10:41:14 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,18 +1,18 @@
 public class Game
 {
-        public void promptForInput()
+	public void promptForInput()
     {
         //Declare and initialize a scanner object called keyboard
-        Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
         String userEntry = "";
-        while(! userEntry.equals("*"))
-        {
+		userEntry = keyboard.nextLine();
+		Board gameBoard = new Board(userEntry);
+        while(!userEntry.equals("*")){
             System.out.print("\f");
             System.out.print(userEntry);
             System.out.print("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
             userEntry = keyboard.nextLine();
-            //convert userEntry to be trimmed and all upper case
-            userEntry = userEntry.trim().toUpperCase();
+            userEntry = userEntry.trim().toUpperCase();//convert userEntry to be trimmed and all upper case
         }
         System.out.print("\nGood Bye!");
     }  
