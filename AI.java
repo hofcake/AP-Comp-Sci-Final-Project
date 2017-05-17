0f2ecaca57f3e423f96bc4e544f14a534ebc50c6
jacob3578 <jessig@pennington.org>
2017-05-17 13:25:45 -0400

    Update AI.java

--- AI.java
+++ AI.java
@@ -6,7 +6,13 @@ public class AI
 	}
 	
 public void defence(){
-	gameBoard.checkAlmost(1);
+	if (gameBoard.checkForAlmost(1) != 0){
+		int coordinates = gameBoard.checkForAlmost(1);
+		int x = coordinates.substring(0, 1);
+		int y = coordinates.substring(1, 2);
+		
+		}
+	
 	
 }
 	
