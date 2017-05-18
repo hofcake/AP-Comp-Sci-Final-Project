3f3d0cb516e54ac1e3490aeed01860a3a5b57a76
Jamie Hofer <jamie.k.hofer@gmail.com>
2017-05-18 14:10:56 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,26 +4,26 @@ public class Game
 	public void promptForInput(){
 		boolean acceptable = false;
 		Scanner keyboard = new Scanner(System.in);//Declare and initialize a scanner object called keyboard
-    	String userEntry = "";
+		String userEntry = "";
 		userEntry = keyboard.nextLine();
 		Board gameBoard = new Board(userEntry);
-        while(!userEntry.equals("*")){
+		while(!userEntry.equals("*")){
 			while (acceptable != true){
-            	System.out.print("\f");
-            	System.out.print(userEntry);
-            	System.out.print("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
-            	userEntry = keyboard.nextLine();
+				System.out.print("\f");
+				System.out.print(userEntry);
+				System.out.print("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
+				userEntry = keyboard.nextLine();
 				coordArray = userEntry.split(",");
-					for (x = 0; x <=1; x ++){
-						if(coordArray[x] > gameBoard.size()){
-							System.out.println("These coordinates do not exist. Please enter new coordinates");
-						}
+				for (x = 0; x <=1; x ++){
+					if(coordArray[x] > gameBoard.size()){
+						System.out.println("These coordinates do not exist. Please enter new coordinates");
+					}
 					else{
 						acceptable = true;
 					}
 			}
-        }
-    }
+		}
+	}
 	public void oneGame(int size, boolean aiFirst){ //true is the AI starting first, false is the human
 		Board gameBoard = new Board(size);
 		if(aiFirst){
