e02ae204ca3a0f3bd77e3f675a280a08b2992f68
Jamie Hofer <jamie.k.hofer@gmail.com>
2017-05-18 14:59:36 -0400

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,44 +1,49 @@
 public class AI
 {
-  public int[] play(int[][] board){
-	int[][] gameBoard = board;
-	int coords = 0;
-	int[] coordsArray = new int[2];
-	if (defense(gameBoard) != 0){
-		coords = defense(gameBoard);
-	}
-	else if (offense(gameBoard) != 0){
-		coords = offense(gameBoard);
-	}
-	else{
-		coords = 23;
-	}
-	coordsArray = makeCoordArray(coords);
-	return coordsArray;
-}
-	
-	public int defense(int[][] gameBoard){
-	if (gameBoard.checkForAlmost(1) != 0){
-		int coordinates = gameBoard.checkForAlmost(1);
-		return coordinates;
-	}
-		else{
-			return 0;}
-}
-	
-	public int offense(int[][] gameBoard){
-	if (gameBoard.checkForAlmost(2) != 0){
-		int coordinates = gameBoard.checkForAlmost(2);
-		return coordinates;
-	}
-		else{
-			return 0;}
-}
-	
-	public int[] makeCoordArray(int coordinates){
-		int[] coordinateArray = new int[2];
-		coordinateArray[0] = coordinates.substring(0,1);
-		coordinateArray[1] = coordinates.substring(1,2);
-		return coordinateArray;
-		}
+    Board gameBoard;
+    public AI (Board gb)
+    {
+        gameBoard = gb;
+    }
+
+    public int[] play(){
+        int coords = 0;
+        int[] coordsArray = new int[2];
+        if (defense(gameBoard) != 0){
+            coords = defense(gameBoard);
+        }
+        else if (offense(gameBoard) != 0){
+            coords = offense(gameBoard);
+        }
+        else{
+            coords = 23;
+        }
+        coordsArray = makeCoordArray(coords);
+        return coordsArray;
+    }
+
+    public int defense(){
+        if (gameBoard.checkForAlmost(1) != 0){
+            int coordinates = gameBoard.checkForAlmost(1);
+            return coordinates;
+        }
+        else{
+            return 0;}
+    }
+
+    public int offense(){
+        if (gameBoard.checkForAlmost(2) != 0){
+            int coordinates = gameBoard.checkForAlmost(2);
+            return coordinates;
+        }
+        else{
+            return 0;}
+    }
+
+    public int[] makeCoordArray(int coordinates){
+        int[] coordinateArray = new int[2];
+        coordinateArray[0] = coordinates.substring(0,1);
+        coordinateArray[1] = coordinates.substring(1,2);
+        return coordinateArray;
+    }
 }
