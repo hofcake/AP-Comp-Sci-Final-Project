2e8f1cf3e7614ed55bec5f4076296a2d46954dff
Jamie Hofer <jamie.k.hofer@gmail.com>
2017-05-22 08:55:23 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,50 +1,55 @@
+import java.util.*;
 public class Game
 {
-	public int[] promptForInput(){
-		int[] coordArray = new int[2];
-		boolean acceptable = false;
-		Scanner keyboard = new Scanner(System.in);//Declare and initialize a scanner object called keyboard
-		String userEntry = "";
-		userEntry = keyboard.nextLine();
-		Board gameBoard = new Board(userEntry);
-		while(!userEntry.equals("*")){
-			while (acceptable != true){
-				System.out.print("\f");
-				System.out.print(userEntry);
-				System.out.print("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
-				userEntry = keyboard.nextLine();
-				coordArray = userEntry.split(",");
-				for (x = 0; x <=1; x ++){
-					if(coordArray[x] > gameBoard.size()){
-						System.out.println("These coordinates do not exist. Please enter new coordinates");
-					}
-					else{
-						acceptable = true;
-						return coordArray;
-					}
-			}
-		}
-	}
-	}
-	public void oneGame(int size, boolean aiFirst){ //true is the AI starting first, false is the human
-		Board gameBoard = new Board();
-		gameBoard.newBoard(size);
-		AI theAI = new AI(gameBoard);
-		if(aiFirst){
-			while(!gameBoard.isWinner()){
-				gameBoard.printBoard();
-				gameBoard.newArrayMark(AI.play(), 2);
-				gameBoard.printBoard();
-				gameBoard.newArrayMark(promptForInput(),1);
-			}
-		}
-		else{
-			while(!gameBoard.isWinner()){
-				gameBoard.printBoard();
-				gameBoard.newArrayMark(promptForInput(),1);
-				gameBoard.printBoard();
-				gameBoard.newArrayMark(AI.play(), 2);		
-			}
-		}
-	}
+    public int[] promptForInput(Board gameBoard){
+        int[] coordArray = new int[2];
+        boolean acceptable = false;
+        Scanner keyboard = new Scanner(System.in);//Declare and initialize a scanner object called keyboard
+        String userEntry = "";
+        String[] entryArray;
+        userEntry = keyboard.nextLine();
+        while(!userEntry.equals("*")){
+            while (acceptable != true){
+                System.out.print("\f");
+                System.out.print(userEntry);
+                System.out.print("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
+                userEntry = keyboard.nextLine();
+                entryArray = userEntry.split(",");
+                for(int i = 0; i < entryArray.length; i++){
+                    coordArray[i] = Integer.valueOf(entryArray[i]);
+                }
+                for (int x = 0; x <=1; x ++){
+                    if(coordArray[x] > gameBoard.size()){
+                        System.out.println("These coordinates do not exist. Please enter new coordinates");
+                    }
+                    else{
+                        acceptable = true;
+                        return coordArray;
+                    }
+                }
+            }
+        }
+        return coordArray;
+    }
+    public void oneGame(int size, boolean aiFirst){ //true is the AI starting first, false is the human
+        Board gameBoard = new Board();
+        gameBoard.newBoard(size);
+        AI theAI = new AI(gameBoard);
+        if(aiFirst){
+            while(!gameBoard.isWinner()){
+                gameBoard.printBoard();
+                gameBoard.newArrayMark(theAI.play(), 2);
+                gameBoard.printBoard();
+                gameBoard.newArrayMark(promptForInput(gameBoard), 1);
+            }
+        }
+        else{
+            while(!gameBoard.isWinner()){
+                gameBoard.printBoard();
+                gameBoard.newArrayMark(promptForInput(gameBoard),1);
+                gameBoard.printBoard();
+                gameBoard.newArrayMark(theAI.play(), 2);       
+            }
+        }
+    }
 }
