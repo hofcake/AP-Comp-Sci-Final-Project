b89cef2378b2636b21493cc4fbedfdd38d3c43d8
jacob3578 <jessig@pennington.org>
2017-05-25 12:17:32 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -51,6 +51,61 @@ public class Game
             }
         }
         gameBoard.printBoard();
-        System.out.print("You win");
+        import java.util.*;
+public class Game
+{ 
+    Scanner keyboard = new Scanner(System.in);
+    public int[] promptForInput(Board gameBoard){
+        int[] coordArray = new int[2];
+        boolean acceptable = false;
+        String userEntry = "";
+        String[] entryArray;
+        while (acceptable == false){
+            acceptable == true;
+            System.out.println("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
+            userEntry = keyboard.nextLine();
+            entryArray = userEntry.split(",");
+            for(int i = entryArray.length - 1; i >= 0; i--){
+                coordArray[i] = Integer.valueOf(entryArray[i]); //Convert to Integer
+            }
+            if(coordArray[0] > gameBoard.size()-1){
+                acceptable = false;
+                System.out.println("Your X cordinate was invalid");
+            }
+            if(coordArray[1] > gameBoard.size()-1){
+                acceptable = false;
+                System.out.println("Your Y conrdinate was invalid");
+            }
+            if(!(gameBoard.checkMark(coordArray[0],coordArray[1]))){
+                acceptable = false;
+                System.out.println("There is already a mark there");
+            }
+        }
+        return coordArray;
+    }
+
+    public void oneGame(int size, boolean aiFirst){ //true is the AI starting first, false is the human
+        Board gameBoard = new Board();
+        gameBoard.newBoard(size);
+        AI theAI = new AI(gameBoard);
+        if(aiFirst){
+            while(!gameBoard.isWinner()){
+                gameBoard.newArrayMark(theAI.play(), 2);
+                gameBoard.printBoard();
+                gameBoard.newArrayMark(promptForInput(gameBoard), 1);
+            }
+        }
+        else{
+            while(!gameBoard.isWinner()){
+                gameBoard.printBoard();
+                gameBoard.newArrayMark(promptForInput(gameBoard),1);
+                gameBoard.newArrayMark(theAI.play(), 2);
+            }
+        }
+        gameBoard.printBoard();
+        int winner = gameBoard.getWinner();
+        System.out.print("you win");
+    }
+}
     }
 }
