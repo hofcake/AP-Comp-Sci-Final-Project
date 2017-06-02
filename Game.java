50073e0150f59d5e9f627bc269bca38d9c3fef78
jacob3578 <jessig@pennington.org>
2017-06-02 09:31:40 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,6 +8,7 @@ public class Game
         System.out.print("\f");
         System.out.println("How many points to a win?");
         int winTotal = Integer.valueOf(keyboard.nextLine());
+        System.out.println("");
         System.out.println("How big would you like the board");
         int size = Integer.valueOf(keyboard.nextLine());
         while((numberWins < winTotal) || (numberAIWins < winTotal))
@@ -17,6 +18,7 @@ public class Game
         else
             System.out.println("You Lost The Game :P");
     }
+
     public int[] promptForInput(Board gameBoard){
         int[] coordArray = new int[2];
         boolean acceptable = false;
@@ -24,34 +26,35 @@ public class Game
         String[] entryArray;
         while (acceptable == false){
             acceptable = true;
-            System.out.println("Enter the x and y coordinates of the space you would like to fill or * to exit : ");
+            System.out.println("Enter the X and Y coordinates of the space you would like to fill: ");
             userEntry = keyboard.nextLine();
+            userEntry.replaceAll("\\s","");
             entryArray = userEntry.split(",");
             for(int i = entryArray.length - 1; i >= 0; i--){
                 try{coordArray[i] = Integer.valueOf(entryArray[i]);}
                 catch(NumberFormatException nfe){
-                    System.out.print("/n");
+                    System.out.print("\n");
                     gameBoard.printBoard();
                     System.out.println("Please enter valid coordinates");
                     coordArray = promptForInput(gameBoard);}
             }
             if(coordArray[0] > gameBoard.size()-1){
                 acceptable = false;
-                System.out.print("/n");
+                System.out.print("\n");
                 gameBoard.printBoard();
-                System.out.println("Your X cordinate was invalid");
+                System.out.println("Your X coordinate was invalid");
                 coordArray = promptForInput(gameBoard);
             }
             if(coordArray[1] > gameBoard.size()-1){
                 acceptable = false;
-                System.out.print("/n");
+                System.out.print("\n");
                 gameBoard.printBoard();
-                System.out.println("Your Y conrdinate was invalid");
+                System.out.println("Your Y coordinate was invalid");
                 coordArray = promptForInput(gameBoard);
             }
             if(!(gameBoard.checkMark(coordArray[0],coordArray[1]))){
                 acceptable = false;
-                System.out.print("/n");
+                System.out.print("\n");
                 gameBoard.printBoard();
                 System.out.println("There is already a mark there");
                 coordArray = promptForInput(gameBoard);
@@ -76,22 +79,22 @@ public class Game
             if(counter%2 == 0)
                 gameBoard.newArrayMark(promptForInput(gameBoard),1);
             else
-                gameBoard.newArrayMark(theAI.play(), 2);
+                gameBoard.newAIArrayMark(theAI.play(), 2);
             gameBoard.printBoard();
             counter++;
         }
         if(gameBoard.getWinner() == 1){
-            System.out.println("You Won This Round!!!!\nPress Enter To Continue");
+            System.out.println("You Won This Round!\nPress Enter To Continue");
             keyboard.nextLine();
             numberWins++;
         }
         else if(gameBoard.getWinner() == 2){
-            System.out.println("You Lost :P\nPress Enter To Continue");
+            System.out.println("You Lost This Round!\nPress Enter To Continue");
             keyboard.nextLine();
             numberAIWins++;
         }
         else{
-            System.out.println("tie\nPress Enter To Continue");
+            System.out.println("You Tied The AI This Round!\nPress Enter To Continue");
             keyboard.nextLine();
         }
         gameBoard.printBoard();
